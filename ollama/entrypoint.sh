ef2d06323d8f1a11a6c92351afaed6d3c5b980d7
Valeriy <lev.370x@gmail.com>
2025-04-17 18:02:09 +0200

    Ollama pulling is working

--- ollama/entrypoint.sh
+++ ollama/entrypoint.sh
@@ -3,9 +3,11 @@ set -e
 
 # Start Ollama in background
 ollama serve &
+# Process id
+pid=$!
 
 # Wait for server initialization
-sleep 2
+sleep 3
 
 # Pull models. Place models here
 ollama pull snowflake-arctic-embed
@@ -13,5 +15,5 @@ ollama pull nomic-embed-text
 ollama pull llama3
 ollama pull mistral
 
-# Keep container running
-wait
+# Keep container running, wait for ollama process to finish
+wait $pid
