58746821bb1a456f181ca7e698b16a314864a2e6
Valeriy <lev.370x@gmail.com>
2025-04-21 22:26:15 +0200

    models

--- ollama/entrypoint.sh
+++ ollama/entrypoint.sh
@@ -10,10 +10,8 @@ pid=$!
 sleep 3
 
 # Pull models. Place models here
-ollama pull snowflake-arctic-embed
-ollama pull nomic-embed-text
+ollama pull jeffh/intfloat-multilingual-e5-large:f16
 ollama pull llama3
-ollama pull mistral
 
 # Keep container running, wait for ollama process to finish
 wait $pid
